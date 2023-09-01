f5e05899e7671d68cc27b7b7968bbff6b5373d86
Rodrigo Carvalho <54452396+Rodrigokrvalho@users.noreply.github.com>
2023-09-01 16:33:59 -0300

    seo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import { globalStyles } from '@/styles/global';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/reacy-query';
+import { DefaultSeo } from 'next-seo';
 
 globalStyles();
 
@@ -11,6 +12,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={pageProps.session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.url.ie/',
+            siteName: 'Ignite Call',
+          }}
+          defaultTitle='Ignite Call'
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,23 +3,30 @@ import previewImage from '../../assets/calendar.png';
 import Image from 'next/image';
 import { Container, Hero, Preview } from './styles';
 import { ClaimUsernameForm } from './components/ClaimUsernameForm';
+import { NextSeo } from 'next-seo';
 
 export default function Index() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl" as="h1">
-          Agendamento descomplicado
-        </Heading>
-        <Text size="lg">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
+    <>
+      <NextSeo
+        title="Agendamento descomplicado"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl" as="h1">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="lg">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
 
-      <Preview>
-        <Image src={previewImage} alt="Agendamento" height={400} quality={100} priority />
-      </Preview>
-    </Container>
+        <Preview>
+          <Image src={previewImage} alt="Agendamento" height={400} quality={100} priority />
+        </Preview>
+      </Container>
+    </>
   );
 }
